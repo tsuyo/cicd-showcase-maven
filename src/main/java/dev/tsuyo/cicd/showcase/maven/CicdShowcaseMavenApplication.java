08e332b743b1c6a7ca6e2f07d04556298d868685
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-10-23 21:10:20 +0900

    initial

---
+++ src/main/java/dev/tsuyo/cicd/showcase/maven/CicdShowcaseMavenApplication.java
@@ -0,0 +1,13 @@
+package dev.tsuyo.cicd.showcase.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdShowcaseMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdShowcaseMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tsuyo/cicd/showcase/maven/CicdShowcaseMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tsuyo.cicd.showcase.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdShowcaseMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
